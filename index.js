145601d4a4b15fb9be187c3cff9db5b27d10481a
Jukka Vilhunen <jukka.vilhunen@hotmail.com>
2023-08-26 20:45:11 +0300

    mongoose added

--- index.js
+++ index.js
@@ -2,8 +2,9 @@ const express = require('express')
 const morgan = require('morgan')
 const app = express()
 const cors = require('cors')
+require('dotenv').config()
 
-
+const Person = require('./models/person')
 
 /* Middleware, joka tulostaa konsoliin palvelimelle tulevien POST pyyntöjen tiedot */
 const requestLogger = (request, response, next) => {
@@ -27,6 +28,16 @@ Helena Takalo
 040-666622
 */
 
+const errorHandler = (error, request, response, next) => {
+  console.error(error.message)
+
+  if (error.name === 'CastError') {
+    return response.status(400).send({ error: 'malformatted id' })
+  }
+
+  next(error)
+}
+
 const unknownEndpoint = (request, response) => {
   response.status(404).send({ error: 'unknown endpoint' })
 }
@@ -42,75 +53,13 @@ app.use(morgan(':method :url :status :response-time ms - :res[content-length] :b
 /* GET /api/persons 200 102.905 ms - 329 {} - -  */
 /* POST /api/persons 200 48.755 ms - 64 {"name":"Helena Takalo","number":"040-666622"} - 72 */
 
-
 //app.use(morgan('tiny'))
-
-//morgan(':method :url :status :res[content-length] - :response-time ms');
-
-
-/*
-morgan.token('host', function(req, res) {
-    return req.hostname;
-});
-
-morgan.token('param', function(req, res, param) {
-    return req.params[param];
-});
-
-app.use(morgan(':method :host :status :param[id] :res[content-length] - :response-time ms'));
-*/
-
-/*
-if (req.method === "POST") {
- app.use((req, res, next) => {
-    console.log(req.body);
-    next();
-  });
-}
-*/
-
-/*
- app.use((req, res, next) => {
-    console.log(req.body);
-    next();
-  });
-*/
-
 
 app.use(requestLogger)
 app.use(express.static('dist'))
 
-let persons = [
-    {
-      id: 1,
-      name: "Arto Hellas",
-      number: "040-123456"
-    },
-    {
-      id: 2,
-      name: "Ada Lovelace",
-      number: "39-44-5323523"
-    },
-    {
-      id: 3,
-      name: "Dan Abramov",
-      number: "12-43-234345"
-    },
-    {
-      id: 4,
-      name: "Mary Poppendieck",
-      number: "39-23-6423122"
-    },
-    {
-      id: 5,
-      name: "Jukka Vilhunen",
-      number: "050-3002111"
-    }
-]
-
-
-
 /* Route. Web page request main page */
+/* 
 app.get('/', (req, res) => {
   const ddate = new Date()
   const mnum = generateId()
@@ -118,31 +67,36 @@ app.get('/', (req, res) => {
   console.log('rnum = ', rnum, 'mnum = ', mnum, 'ddate =', ddate)
   res.send(`<h2>Hello World. Random number is ${rnum} and Max number is ${mnum} and date is ${ddate}</h2>`)
 })
+*/
+
 
 /* Route. Web page request all phonebook data */
-app.get('/api/persons', (req, res) => {
-  res.json(persons)
+app.get('/api/persons', (request, response) => {
+  Person.find({}).then(persons => {
+    response.json(persons)
+  })
 })
 
 /* Route. Web page request single (one) phonebook data */
-app.get('/api/persons/:id', (request, response) => {
-  const id = Number(request.params.id)
-  const person = persons.find(person => person.id === id)
-
-  if (person) {
-    response.json(person)
-  } else {
-    response.status(404).end()
-  }
-
+app.get('/api/persons/:id', (request, response, next) => {
+  Person.findById(request.params.id)
+    .then(person => {
+      if (person) {
+        response.json(person)
+      } else {
+        response.status(404).end()
+      }
+    })
+    .catch(error => next(error))
 })
 
 /* Web page request to delete single (one) phonebook data */
-app.delete('/api/persons/:id', (request, response) => {
-  const id = Number(request.params.id)
-  persons = persons.filter(person => person.id !== id)
-
-  response.status(204).end()
+app.delete('/api/persons/:id', (request, response, next) => {
+  Person.findByIdAndRemove(request.params.id)
+    .then(result => {
+      response.status(204).end()
+    })
+    .catch(error => next(error))
 })
 
 /* Route. Web page request to add single (one) phonebook data */
@@ -164,23 +118,42 @@ app.post('/api/persons', (request, response) => {
     })
   }
 
-  if (persons.filter(e => e.name === body.name).length > 0) {
+/*
+
+  Person.find({}).then(persons => {
+  if (person.filter(e => e.name === body.name).length > 0) {
     return response.status(400).json({ 
       error: `${body.name} is already added to phonebook` 
     })
   }
+  })
+*/
 
-  const person = {    
+  const person = new Person({
     name: body.name,
-    number: body.number,
-    id: randomNumberInRange(10, 100)
-  }
+    number: body.number
+  })
 
-  persons = persons.concat(person)
-
-  response.json(person)
+  person.save().then(savedPerson => {
+    response.json(savedPerson)
+  })
 })
 
+
+app.put('/api/persons/:id', (request, response, next) => {
+  const body = request.body
+
+  const person = {
+    name: body.name,
+    number: body.number,
+  }
+
+  Person.findByIdAndUpdate(request.params.id, person, { new: true })
+    .then(updatedPerson => {
+      response.json(updatedPerson)
+    })
+    .catch(error => next(error))
+})
 
 
 function randomNumberInRange(min, max) {
@@ -196,9 +169,10 @@ const generateId = () => {
 }
 
 app.use(unknownEndpoint)
+app.use(errorHandler)
 
-//const PORT = 3001
-const PORT = process.env.PORT || 3001
+//const PORT = process.env.PORT || 3001
+const PORT = process.env.PORT
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`)
 })
---
+++ models/person.js
@@ -0,0 +1,32 @@
+const mongoose = require('mongoose')
+
+mongoose.set('strictQuery', false)
+
+/* mongoose library cant't read the url from the .env file because there is '&' character in the url */
+//const url = process.env.MONGODB_URI
+const url = `mongodb+srv://jvmongodb:opto4646asdf@cluster0.dvmusb2.mongodb.net/?retryWrites=true&w=majority`
+
+console.log('connecting to', url)
+
+mongoose.connect(url)
+  .then(result => {
+    console.log('connected to MongoDB')
+  })
+  .catch((error) => {
+    console.log('error connecting to MongoDB:', error.message)
+  })
+
+const personSchema = new mongoose.Schema({
+  name: String,
+  number: String
+})
+
+personSchema.set('toJSON', {
+  transform: (document, returnedObject) => {
+    returnedObject.id = returnedObject._id.toString()
+    delete returnedObject._id
+    delete returnedObject.__v
+  }
+})
+
+module.exports = mongoose.model('Person', personSchema)

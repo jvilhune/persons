05d47d74c5ac323f4648c275a1df8845a95c595f
Jukka Vilhunen <jukka.vilhunen@hotmail.com>
2023-08-28 10:04:15 +0300

    custom validation added for phone number

--- models/person.js
+++ models/person.js
@@ -24,10 +24,69 @@ const personSchema = new mongoose.Schema({
   },
   number: {
     type: String,
-    minlength: 5,
-    required: true
+    validate: {
+     validator: function(tstnumberstr) {
+        var result = endFunction(tstnumberstr);
+        return result;
+      },
+      message: props => `${props.value} is not a valid phone number!`
+    },
+    required: [true, 'User phone number required']
   },
 })
+
+
+/*
+IS PHONE NUMBER LEGAL?
+The lenght of the phone number string is at least 8 characters (including '-' chars)
+1st part has 2 or 3 number
+the second part has at least 4 or 5 numbers
+One hyphen character (-) between the first and second part
+09-1234556 is legal
+040-22334455 is legal
+1234556 is illegal
+1-22334455 is illegal
+10-22-334455 is illegal
+*/
+const endFunction = (phonenumber) => {
+  var retval = "";
+  var phonenum = phonenumber;
+  var a = 0;
+  var numcount = 0;
+  var hyphencount = 0;
+  var hyphenindex = 0;
+  var notnumnothypencount = 0;
+  var len = phonenum.length;
+
+  for(a=0;a<phonenum.length;a++)
+  {
+    if(phonenum[a] == '-')
+    {
+      hyphencount++;
+      hyphenindex = a;
+    }
+    else if (phonenum[a] >= '0' && phonenum[a] <= '9')
+    {      
+      numcount++;
+    }
+    else
+    {      
+      notnumnothypencount++;
+    }
+  }
+
+  if(len > 7 && notnumnothypencount == 0 && numcount > 6 && hyphencount == 1 && (hyphenindex == 2 || hyphenindex == 3))
+  {
+    /* Phone number is illegal */
+    retval = 1;
+  }
+  else
+  {
+    /* Phone number is legal */
+    retval = 0;
+  }
+  return retval;
+}
 
 personSchema.set('toJSON', {
   transform: (document, returnedObject) => {
